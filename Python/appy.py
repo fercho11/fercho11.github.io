ca8bf83af39c765f7070e0c2f93676ea25a97f40
fernando <fernando.casta.1103@gmail.com>
2022-10-11 10:21:30 -0500

    Proyecto que implementa crud en Python

--- Python/appy.py
+++ Python/appy.py
@@ -1,34 +1,65 @@
 lista_elements = {
     "id" : "1",
-    "nombre" : "Emmanuel",
-    "apellido" : "Aguilar"
+    "nombre" : "Fernando",
+    "apellido" : "Castañeda"
 },{
     "id" : "2",
-    "nombre" : "Omar",
-    "apellido" : "Flores"
+    "nombre" : "Emmanuel",
+    "apellido" : "Aguilar"
 }
 
 lista_element = []
 
 def add_element():
-    lista_elements 
+   id = int (input('Ingresa el ID de la persona: '))
+   name = input (f"\n Ingresa el nombe de la persona: ")
+   last_name = input (f"\n Ingresa el apellido de la persona: ")
+   person = {
+    "id": id,
+    "name": name,
+    "last_name": last_name
+   }
+   lista_elements.append(person)
+    #lista_elements.append ()
 
 def remove_element():
-    pass
+    #for para buscar (puede buscar con find)
+    #lista_elements.remove
+    id = int (input('Ingresa el ID de la persona a Eliminar: '))
+    found = find_element (id)
+    print (found)
+    aceptar = input("Estás seguro? (S/N)")
+    if aceptar == "S":
+        lista_elements.remove (found)
+        print("Elemento Eliminado")
 
-def show_element():
-    pass
+def find_element ():
+    #for para buscar
+    #return element
+    found = {}
+    for element in lista_elements:
+        if element ['id'] == id:
+            found = element
+    return found
 
 def show_elements():
     # print(lista_element)
+    for element in lista_elements:
+        for key, value in element.items():
+            print (f"{key} -> {value}")
     pass
 
 def edit_element():
-    pass
+    #for o find para buscar
+    #editar
+    id = int (input('Ingresa el ID del elemento a editar: '))
+    found = find_element (id)
+    print(found)
 
 
 
-if _name_ == '_main_':
+
+if __name__ == '__main__':
     menu = '''
     Implementacion de CRUD de Elementos:
     Elige una Opcion
@@ -43,8 +74,10 @@ if _name_ == '_main_':
         opcion = input(menu)
         if opcion == '1':
             print("Insertar Elemento")
+            add_element()
         elif opcion == '2':
             print ("Mostrar todos los elementos")
+            show_elements()
         elif opcion == '3':
             print ("Bucar por ID")
         elif opcion == '4':
